f620e1cf518f43155d988b62fa6baeeab76cff29
Noel Parisi <ycleptnoel@gmail.com>
2017-06-23 19:09:43 -0700

    beginnings of map component

---
+++ src/redux/reducers/location.js
@@ -0,0 +1,11 @@
+import initState from '../initialState'
+import {UPDATE_LOCATION} from '../actions/types'
+
+export default (state = initState.location, {type, payload}) => {
+	switch (type) {
+		case UPDATE_LOCATION:
+			return payload
+		default:
+			return state
+	}
+}
