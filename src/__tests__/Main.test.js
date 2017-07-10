0739ce89692ee6278d6560cc781f7036d70dd3b9
ycleptkellan <yclepkellan@gmail.com>
2017-07-10 13:30:47 -0700

    first tests

---
+++ src/__tests__/Main.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import {shallow, render} from 'enzyme'
+import {Provider} from 'react-redux'
+
+import store from '../redux/store.js'
+
+import {Unwrapped as UnwrappedMain} from '../Main'
+
+test('Main renders correctly', () => {
+  const component = shallow(
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  )
+  expect(component).toMatchSnapshot()
+})
---
+++ src/__tests__/Styled.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+test('Styled works', () => {
+  expect(1).toBe(1)
+})
