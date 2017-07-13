e64cf3424f72b009ac51650b3c0ee7daf771274f
ycleptkellan <yclepkellan@gmail.com>
2017-07-12 17:28:57 -0700

    fix tests

--- src/__tests__/Main.test.js
+++ src/__tests__/Main.test.js
@@ -3,9 +3,9 @@ import renderer from 'react-test-renderer';
 import { shallow, render } from 'enzyme';
 import { Provider } from 'react-redux';
 
-import store from '../redux/store.js';
+import store from '../redux/store';
 
-import { Unwrapped as UnwrappedMain } from '../Main';
+import Main, { Unwrapped as UnwrappedMain } from '../Main';
 
 test('Main renders correctly', () => {
   const component = shallow(
