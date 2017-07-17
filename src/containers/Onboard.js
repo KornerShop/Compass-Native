298e809d9b8b9ae2e4d6f4bce7449461ee70fc32
Noel Parisi <ycleptnoel@gmail.com>
2017-07-17 09:44:52 -0700

    carasol work

--- src/containers/Onboard.js
+++ src/containers/Onboard.js
@@ -1,42 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { shape, number } from 'prop-types';
-import { Text, View } from 'react-native';
-import styled from 'styled-components/native';
+import { Text, View, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+// import { ScreenOrientation } from 'expo';
 import Carousel from 'react-native-looped-carousel';
 
-const OnboardScreen = styled.View`
-  flex:1;
-`
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+//
+// import { updateOrientation } from '../redux/actions/actions';
 
-const Onboard = ({ orientation: { height, width } }) => {
-  const orientationObj = {
-    height,
-    width,
-  };
-  console.warn(JSON.stringify(orientationObj))
-  return (
-    <OnboardScreen>
-      <Carousel
-        style={orientationObj}
-        autoplay={false}
-        bullets
-        onAnimateNextPage={(p) => console.log(p)}
-      >
-        <View
-          style={[{ backgroundColor: '#BADA55' }, orientationObj]}
+import styled from 'styled-components/native';
+
+const OnboardScreen = styled.View`flex: 1;`;
+
+export default class Onboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const orientationObj= {
+      height: this.props.orientation.height,
+      width: this.props.orientation.width
+    }
+    return (
+      <OnboardScreen>
+        <Carousel
+          style={orientationObj}
+          autoplay={false}
+          bullets
+          onAnimateNextPage={p => console.log(p)}
         >
-          <Text>1</Text>
-        </View>
-        <View style={[{ backgroundColor: 'pink' }, orientationObj]}>
-          <Text>2</Text>
-        </View>
-        <View style={[{ backgroundColor: 'blue' }, orientationObj]}>
-          <Text>3</Text>
-        </View>
-      </Carousel>
-    </OnboardScreen>
-  );
-};
+          <View
+            style={[
+              { backgroundColor: 'white' },
+              orientationObj,
+            ]}
+          >
+            <Text>
+              {Platform.OS === 'ios'
+                ? <Ionicons
+                    name="md-compass"
+                    size={160}
+                    color="#00897b"
+                  />
+                : <Ionicons
+                    name="ios-locate"
+                    size={160}
+                    color="#00897b"
+                  />}
+            </Text>
+          </View>
+          <View
+            style={[
+              { backgroundColor: 'white' },
+              orientationObj,
+            ]}
+          >
+            <Text>
+              <Ionicons
+                name="ios-nutrition-outline"
+                size={160}
+                color="#00897b"
+              />
+            </Text>
+          </View>
+          <View
+            style={[
+              { backgroundColor: 'white' },
+              orientationObj,
+            ]}
+          >
+            <Text>
+              <Ionicons
+                name="ios-woman-outline"
+                size={160}
+                color="#00897b"
+              />
+            </Text>
+          </View>
+        </Carousel>
+      </OnboardScreen>
+    );
+  }
+}
 
 Onboard.propTypes = {
   orientation: shape({
@@ -46,5 +93,3 @@ Onboard.propTypes = {
     fontScale: number,
   }).isRequired,
 };
-
-export default Onboard;
