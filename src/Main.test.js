2b854700111aa4a7288a1342c2cfb7e25978a561
ycleptkellan <yclepkellan@gmail.com>
2017-07-17 20:23:42 -0700

    remove tests

--- src/Main.test.js
+++ src/Main.test.js
@@ -1,48 +1,63 @@
+import { ActivityIndicator } from 'react-native';
 import React from 'react';
-import 'react-native';
 import ReactTestUtils from 'react-dom/test-utils';
+import renderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import { Provider } from 'react-redux';
 
 import Main, { Unwrapped as UnwrappedMain } from './Main';
 import Welcome from './screens/Welcome';
 import NavigationProvider from './containers/NavigationProvider';
 
+import initState from './redux/initialState';
+
 const wait = (exp, ms) =>
   new Promise(resolve => setTimeout(() => resolve(exp), ms));
 
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
 describe('renderer tests', () => {
-  test('UnwrappedMain renders correctly', () => {
+  test('UnwrappedMain renders correctly (shallow)', () => {
     const shallowRenderer = new ShallowRenderer();
     shallowRenderer.render(<UnwrappedMain />);
-    const wrapper = shallowRenderer.getRenderOutput();
-    console.log(`wrapper: ${JSON.stringify(wrapper)}`);
-    expect(wrapper).toMatchSnapshot();
+    const result = shallowRenderer.getRenderOutput();
+    expect(result.props.children).toEqual(
+      <ActivityIndicator
+        animating
+        color="#00897b"
+        hidesWhenStopped
+        size="large"
+      />,
+    );
+    expect(result).toMatchSnapshot();
   });
 
-  xtest('Main renders Welcome after initial load', async () => {
-    const wrapper = shallow(<UnwrappedMain />);
-    await wait(wrapper.setState({ isLoading: false }), 100);
-    expect(wrapper.find(Welcome)).toBeTruthy();
-  });
-
-  xtest('Main renders navigationProvider after language chosen', async () => {
-    const wrapper = shallow(<UnwrappedMain />);
-    await wait(wrapper.setState({ isLoading: false }), 100);
-    wrapper.setState({ started: true });
-    expect(wrapper.find(NavigationProvider)).toBeTruthy();
-  });
-
-  xtest('Main renders navigationProvider after language chosen (event)', async () => {
-    const wrapper = shallow(<UnwrappedMain />);
-    await wait(wrapper.setState({ isLoading: false }), 100);
-    wrapper.find(Welcome).simulate('press', [1])
-    expect(wrapper.find(NavigationProvider)).toBeTruthy();
+  test('Main renders correctly', async () => {
+    const store = mockStore({
+      ...initState,
+      orientation: {
+        width: 411.42857142857144,
+        height: 683.4285714285714,
+        scale: 2.625,
+        fontScale: 1,
+      },
+    });
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Main />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
 
 // tests for content ('compass', button on welcome, spanish if language Spanish elsewhere)
-// make checklist after completing this file ^
 // test to make sure mapstatetoprops and mapdispatchtoprops functions are present
 // redux tests (Redux guide) + props tests for connected export (perhaps related)
 // see 'Main renders correctly', above...needs mock store & provider (see btholt's file)
